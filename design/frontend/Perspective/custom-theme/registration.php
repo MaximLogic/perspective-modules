e5969be5b2d44d001bf1d8dbed6f98ffb23ef695
MaximLogic <logoyda96@gmail.com>
2024-02-21 10:31:54 +0200

    Theme with moved blocks

---
+++ design/frontend/Perspective/custom-theme/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Perspective/custom-theme',
+    __DIR__
+);
